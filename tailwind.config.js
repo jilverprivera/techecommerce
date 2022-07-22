ec6fecdd57610d39c2698b81a0f2296ef76991aa
Jilver Pacheco <jilverrivera1@gmail.com>
2022-07-22 12:07:31 -0500

    tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 
-const {fontFamily, screens} = require('tailwindcss/defaultTheme');
+const { fontFamily, screens } = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './layout/**/*.{ts,tsx}',
-  ],
+  content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
-    screens: {
-      xxs: '270px',
-      xs: '350px',
-      ...screens,
-    },
+    screens: { xxs: '270px', xs: '350px', ...screens },
     extend: {
       fontFamily: {
-        supremeLight: ['Supreme-Light', ...fontFamily.sans],
-        supremeRegular: ['Supreme-Regular', ...fontFamily.sans],
-        supremeMedium: ['Supreme-Medium', ...fontFamily.sans],
-        supremeBold: ['Supreme-Bold', ...fontFamily.sans],
+        textLight: ['ClashGrotesk-Light', ...fontFamily.sans],
+        textRegular: ['ClashGrotesk-Regular', ...fontFamily.sans],
+        textMedium: ['ClashGrotesk-Medium', ...fontFamily.sans],
+        textSemibold: ['ClashGrotesk-Semibold', ...fontFamily.sans],
+        textBold: ['ClashGrotesk-Bold', ...fontFamily.sans],
       },
     },
   },
