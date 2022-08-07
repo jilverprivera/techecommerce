45f8377dc166212c15e7c631806575d0b3eabb88
Jilver Pacheco <jilverrivera1@gmail.com>
2022-08-06 20:31:06 -0500

    add query using swr

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -1,117 +1,95 @@
 import { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
-import Link from 'next/link';
+import useSWR from 'swr';
 
 import { UserContext } from '../context/UserContext';
 
 import Layout from '../components/layout';
+import Loader from 'components/Loader';
 
 import { ProductInterface } from '../interfaces/products';
-import { GetServerSideProps } from 'next';
+import VerticalProductCard from 'components/products/VerticalProductCard';
 
-interface serverProps {
-  product: ProductInterface;
-  products: ProductInterface[];
-}
+const fetcherAll = (args: string) => fetch(args).then((res): Promise<ProductInterface[]> => res.json());
+const fetcherSingle = (args: string) => fetch(args).then((res): Promise<ProductInterface> => res.json());
 
-const ProductDetail = ({ product, products }: serverProps) => {
+const ProductDetail = () => {
+  const { query } = useRouter();
+  const { id } = query;
+
+  const { data, isValidating } = useSWR(`/api/products/${id}`, fetcherSingle);
+  const { data: products } = useSWR('/api/products', fetcherAll);
+
   const [relateds, setRelateds] = useState<ProductInterface[]>([]);
   const { addToCart, addToWishList } = useContext(UserContext);
 
   useEffect(() => {
-    const getRelatedProducts = () => {
-      if (product && products) {
-        const relatedProducts = products.filter(
-          (el) => el.category === product.category && el._id !== product._id
-        );
-        setRelateds(relatedProducts);
-      }
-    };
-    getRelatedProducts();
-  }, [product, products]);
+    if (products && data) {
+      const relateds = products.filter((el) => el.category === data.category && el._id !== data._id);
+      setRelateds(relateds);
+    }
+  }, [products, data, id]);
 
   return (
     <Layout title="Product detail - TechEcommerce">
-      <section className="max-w-screen-xl content w-full mx-auto flex items-center justify-center">
-        <div className="grid grid-cols-2 gap-5">
-          <div className="overflow-hidden">
-            {product?.image && (
-              <Image
-                src={product.image.url}
-                alt={product.product_id}
-                width={1024}
-                height={964}
-                objectFit="cover"
-                quality={100}
-              />
-            )}
-          </div>
-          <div className="flex items-start justify-center flex-col">
-            <h1 className="text-3xl font-supremeMedium">{product?.name}</h1>
-            <span className="text-xs font-supremeLight">Ref: {product?.product_id}</span>
-            <p className="w-4/5 my-5 leading-7">{product?.content}</p>
-            <p>Available: {product?.stock}</p>
-            <p>Solds: {product?.sold}</p>
-            <p>$ {product?.price} usd</p>
-
-            {product && (
-              <div className="mt-20">
-                <button
-                  className="bg-zinc-900 px-10 py-2.5 text-white mr-5 hover:bg-zinc-700 duration-150"
-                  onClick={() => addToCart({ ...product, quantity: 1 })}
-                >
-                  Add to cart
-                </button>
-                <button className=" p-2.5 ">
-                  <span
-                    className="border-b-2 border-zinc-900 pb-1"
-                    onClick={() => addToWishList({ ...product })}
-                  >
-                    Add to wish list
-                  </span>
-                </button>
+      <section className="max-w-screen-xl xs:w-11/12 sm:w-11/12 md:w-11/12 lg:w-11/12 xl:w-11/12 content mx-auto flex items-start justify-center">
+        {isValidating ? (
+          <Loader />
+        ) : (
+          <div className="">
+            <div className="grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2  gap-5">
+              <div className="overflow-hidden">
+                {data?.image && (
+                  <Image
+                    src={data.image.url}
+                    alt={data.product_id}
+                    width={1024}
+                    height={964}
+                    quality={100}
+                    objectFit="contain"
+                  />
+                )}
               </div>
-            )}
-          </div>
-        </div>
-      </section>
-      <section className="max-w-screen-xl w-full mx-auto flex flex-col items-center justify-center">
-        <h2 className="text-2xl mb-5">Some products related</h2>
-        <div className="grid grid-cols-4 gap-5">
-          {relateds.slice(0, 4).map((product) => (
-            <Link href={`/${product._id}`} key={product._id}>
-              <a className="border-2 border-zinc-100 flex flex-col items-center justify-center">
-                <Image
-                  className="w-1/4 h-1/4 aspect-square"
-                  src={product.image.url}
-                  alt={product.product_id}
-                  width={680}
-                  height={680}
-                />
-                <div className="my-2.5">
-                  <h4 className="text-xl font-supremeMedium">{product.name}</h4>
-                  <span className="text-sm">${product.price} usd</span>
+              <div className="flex items-start justify-center flex-col">
+                <h1 className="text-3xl font-semibold">{data?.name}</h1>
+                <span className="text-xs font-supremeLight">Ref: {data?.product_id}</span>
+                <p className="w-full my-5 leading-7">{data?.content}</p>
+                <div className="flex mb-3 ">
+                  <p className="text-zinc-500 text-sm mr-3">Available: {data?.stock}</p>
+                  <p className="text-zinc-500 text-sm">Solds: {data?.sold}</p>
                 </div>
-              </a>
-            </Link>
-          ))}
-        </div>
+                <p className="text-2xl font-bold">$ {data?.price}</p>
+                {data && (
+                  <div className="xs:mt-5 sm:mt-20 md:mt-5 lg:mt-20 xl:mt-20">
+                    <button
+                      className="bg-zinc-900 px-10 py-2.5 text-white mr-5 hover:bg-zinc-700 duration-150"
+                      onClick={() => addToCart({ ...data, quantity: 1 })}
+                    >
+                      Add to cart
+                    </button>
+                    <button className=" p-2.5 ">
+                      <span className="border-b-2 border-zinc-900 pb-1" onClick={() => addToWishList({ ...data })}>
+                        Add to wish list
+                      </span>
+                    </button>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div className="w-full flex items-center justify-center flex-col mt-10">
+              <h2 className="text-2xl mb-5">Some products related</h2>
+              <div className="grid xs:grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5">
+                {relateds.slice(0, 4).map((product) => (
+                  <VerticalProductCard key={product._id} {...product} />
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
       </section>
     </Layout>
   );
 };
 
 export default ProductDetail;
-
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  const singleProductReq = await fetch(`http://localhost:3000/api/products/${params?.id}`);
-  const productsReq = await fetch('http://localhost:3000/api/products');
-  const singleProductRes = await singleProductReq.json();
-  const productsRes = await productsReq.json();
-  return {
-    props: {
-      product: singleProductRes,
-      products: productsRes,
-    },
-  };
-};
