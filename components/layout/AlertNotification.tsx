3dd085cea9f3a90c65e37d1cd4066f9fd2c6e08f
Jilver Pacheco <jilverrivera1@gmail.com>
2022-08-06 20:29:52 -0500

    add alert notification message

--- components/layout/AlertNotification.tsx
+++ components/layout/AlertNotification.tsx
@@ -4,30 +4,33 @@ import { FiCheck, FiX } from 'react-icons/fi';
 
 interface Props {
   message: string;
-  type: AlertTypes;
+  type: AlertTypes | null;
 }
 const AlertNotification = ({ message, type }: Props) => (
   <motion.div
-    className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-60 z-50"
+    className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-900 bg-opacity-60 backdrop-blur-sm z-50"
     initial={{ opacity: 0 }}
     animate={{ opacity: 1 }}
     exit={{ opacity: 0 }}
     transition={{ duration: 0.25 }}
   >
     <motion.div
-      className="bg-white flex flex-col justify-center items-center p-20 rounded-xl"
+      className="bg-white flex flex-col justify-center items-center h-52 w-80 rounded-xl"
       initial={{ opacity: 0, y: -20 }}
       animate={{ opacity: 1, y: 0, transition: { delay: 0.25 } }}
       exit={{ opacity: 0, y: 20 }}
     >
-      <span
-        className={`bg-gray-200 w-20 h-20 rounded-full  flex items-center justify-center text-9xl ${
-          type === AlertTypes.ERROR ? 'text-red-500' : 'text-lime-400'
+      <motion.span
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0, transition: { type: 'spring', stiffness: 100, duration: 0.5 } }}
+        exit={{ opacity: 0, y: 20 }}
+        className={`bg-gray-200 text-white w-20 h-20 rounded-full  flex items-center justify-center text-7xl ${
+          type === AlertTypes.ERROR ? 'bg-red-500' : 'bg-lime-400'
         }`}
       >
         {type === AlertTypes.ERROR ? <FiX /> : <FiCheck />}
-      </span>
-      <span className="text-black">{message}</span>
+      </motion.span>
+      <span className="text-black font-textMedium  mt-3">{message}</span>
     </motion.div>
   </motion.div>
 );
