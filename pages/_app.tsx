3f73929514010c42d5035e125285cf51c99a142d
Jilver Pacheco <jilverrivera1@gmail.com>
2022-08-06 20:34:22 -0500

    add layout provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,26 +6,26 @@ import { AppProvider } from 'context/AppContext';
 import { LayoutProvider } from 'context/LayoutContext';
 import { UserProvider } from 'context/UserContext';
 import Header from 'components/layout/Header';
+import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 import 'styles/globals.css';
-// import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    // <PayPalScriptProvider options={{ 'client-id': String(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID) }}>
-    <AppProvider>
-      <UserProvider>
-        <AdminProvider>
-          <LayoutProvider>
-            {router.pathname.includes('/sign') ? <div></div> : <Header />}
-            <AnimatePresence exitBeforeEnter>
-              <Component {...pageProps} key={router.asPath} />
-            </AnimatePresence>
-          </LayoutProvider>
-        </AdminProvider>
-      </UserProvider>
-    </AppProvider>
-    // </PayPalScriptProvider>
+    <PayPalScriptProvider options={{ 'client-id': String(process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID) }}>
+      <AppProvider>
+        <LayoutProvider>
+          <UserProvider>
+            <AdminProvider>
+              {router.pathname.includes('/sign') ? <div></div> : <Header />}
+              <AnimatePresence exitBeforeEnter initial={false}>
+                <Component {...pageProps} key={router.asPath} />
+              </AnimatePresence>
+            </AdminProvider>
+          </UserProvider>
+        </LayoutProvider>
+      </AppProvider>
+    </PayPalScriptProvider>
   );
 }
 
